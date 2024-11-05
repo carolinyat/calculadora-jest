4aef61d1db9d6a08dc72895ecbd61387c8ecd036
HumbertoGFS <humbertogfs55@proton.me>
2024-11-05 17:27:26 -0300

    adicionado teste de falha da adição

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -4,3 +4,8 @@ const { add } = require('../src/calculator');
 test('adds 2 + 3 to equal 5', () => {
     expect(add(2, 3)).toBe(5);
 });
+
+// Teste falha da função de adição
+test('adds 2 + 3 to not equal 4', () => {
+    expect(add(2, 3)).not.toBe(4);
+});
